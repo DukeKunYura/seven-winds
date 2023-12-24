b6917fdc05977c688044f5fe5fb6893717e53b5a
dukekunyura <dukekunyura@gmail.com>
2023-12-25 00:40:21 +0300

    [+] add tree

--- src/components/mainTable/MainTable.tsx
+++ src/components/mainTable/MainTable.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import styles from "./MainTable.module.scss";
 import { useGetPostsQuery } from "../../api/windsApi";
+import { PiFileTextFill } from "react-icons/pi";
 
 export const MainTable: FC = () => {
   const { data: posts } = useGetPostsQuery();
@@ -11,6 +12,73 @@ export const MainTable: FC = () => {
       {posts?.map((item) => (
         <div>{item.rowName}</div>
       ))}
+
+      <ul className={styles.tree}>
+        <li>
+          <div className={styles.item}>
+            <PiFileTextFill color="6287A7" size="1.5em" />
+          </div>
+        </li>
+        <li>
+          <div className={styles.item}>
+            <PiFileTextFill color="6287A7" size="1.5em" />
+          </div>
+        </li>
+        <li>
+          <div className={styles.item}>
+            <PiFileTextFill color="6287A7" size="1.5em" />
+          </div>
+        </li>
+        <li>
+          <div className={styles.item}>
+            <PiFileTextFill color="6287A7" size="1.5em" />
+          </div>
+          <ul>
+            <li>
+              <div className={styles.item}>
+                <PiFileTextFill color="6287A7" size="1.5em" />
+              </div>
+            </li>
+            <li>
+              <div className={styles.item}>
+                <PiFileTextFill color="6287A7" size="1.5em" />
+              </div>
+              <ul>
+                <li>
+                  <div className={styles.item}>
+                    <PiFileTextFill color="6287A7" size="1.5em" />
+                  </div>
+                </li>
+                <li>
+                  <div className={styles.item}>
+                    <PiFileTextFill color="6287A7" size="1.5em" />
+                  </div>
+                </li>
+                <li>
+                  <div className={styles.item}>
+                    <PiFileTextFill color="6287A7" size="1.5em" />
+                  </div>
+                </li>
+                <li>
+                  <div className={styles.item}>
+                    <PiFileTextFill color="6287A7" size="1.5em" />
+                  </div>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <div className={styles.item}>
+                <PiFileTextFill color="6287A7" size="1.5em" />
+              </div>
+            </li>
+            <li>
+              <div className={styles.item}>
+                <PiFileTextFill color="6287A7" size="1.5em" />
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
   );
 };
