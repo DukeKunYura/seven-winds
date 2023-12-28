465a255029324a73917122aedbee9d772e945e06
dukekunyura <dukekunyura@gmail.com>
2023-12-28 09:58:02 +0300

    [*] fix bug with state in OptionsTree

--- src/components/optionsTree/OptionsTree.tsx
+++ src/components/optionsTree/OptionsTree.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactElement, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import styles from "./OptionsTree.module.scss";
 import { PiFileTextFill } from "react-icons/pi";
 import { RiDeleteBinFill } from "react-icons/ri";
@@ -15,11 +15,9 @@ type Props = {
 };
 export const OptionsTree: FC<Props> = ({ rows }) => {
   const active = useAppSelector((state) => state.master.optionsActive);
-  const idEditingRow = useAppSelector((state) => state.master.idEditingRow);
   const dispatch = useAppDispatch();
   const [isActiveAdder, setIsActiveAdder] = useState(false);
   const [items, setItems] = useState(rows);
-  const [renderElements, setRenderElements] = useState<ReactElement>();
   const [deleteRow] = useDeleteRowMutation();
 
   const handleHover = () => {
@@ -30,8 +28,8 @@ export const OptionsTree: FC<Props> = ({ rows }) => {
     dispatch(setActiveOptions(false));
   };
 
-  const handleAdder = async (id: number) => {
-    await dispatch(setIdEditingRow(id));
+  const handleAdder = (id: number) => {
+    dispatch(setIdEditingRow(id));
 
     const adder: IRow = {
       child: [],
@@ -49,55 +47,54 @@ export const OptionsTree: FC<Props> = ({ rows }) => {
       total: 0,
     };
     if (!isActiveAdder) {
-      const newItems = await items.map((item) => {
-        if (item.id === idEditingRow) {
-          setIsActiveAdder(true);
-          return { ...item, child: [...item.child, adder] };
-        } else {
-          return item;
-        }
-      });
-      setItems(newItems);
+      setItems(
+        items.map((item) => {
+          if (item.id === id) {
+            setIsActiveAdder(true);
+            return { ...item, child: [...item.child, adder] };
+          } else {
+            return item;
+          }
+        })
+      );
     }
   };
 
   const treeMapper = (arr: any[]) => {
-    const mapped = arr.map((item: any) => (
-      <li key={item.id}>
-        <div
-          onMouseLeave={handleLeave}
-          className={active ? styles.option : styles.item}
-        >
-          <div className={active ? styles.optionActive : styles.notActive}>
-            <PiFileTextFill
-              onClick={() => {
-                handleAdder(item.id);
-              }}
-              onMouseEnter={handleHover}
-              color="6287A7"
-              size="1.5em"
-            />
-            {active && (
-              <RiDeleteBinFill
-                onClick={() => {
-                  deleteRow(item.id);
-                }}
-                color="#da3535"
-                size="1.3em"
-              />
-            )}
-          </div>
-        </div>
-        {item.child.length ? treeMapper(item.child) : null}
-      </li>
-    ));
-    return <ul className={styles.tree}>{mapped}</ul>;
+    return (
+      <ul className={styles.tree}>
+        {arr.map((item: any) => (
+          <li key={item.id}>
+            <div
+              onMouseLeave={handleLeave}
+              className={active ? styles.option : styles.item}
+            >
+              <div className={active ? styles.optionActive : styles.notActive}>
+                <PiFileTextFill
+                  onClick={() => {
+                    handleAdder(item.id);
+                  }}
+                  onMouseEnter={handleHover}
+                  color="6287A7"
+                  size="1.5em"
+                />
+                {active && (
+                  <RiDeleteBinFill
+                    onClick={() => {
+                      deleteRow(item.id);
+                    }}
+                    color="#da3535"
+                    size="1.3em"
+                  />
+                )}
+              </div>
+            </div>
+            {item.child.length ? treeMapper(item.child) : null}
+          </li>
+        ))}
+      </ul>
+    );
   };
 
-  useEffect(() => {
-    setRenderElements(treeMapper(items));
-    console.log(items);
-  }, [items, active]);
-
-  return <div className={styles.mainTable}>{renderElements}</div>;
+  return <div className={styles.mainTable}>{treeMapper(items)}</div>;
 };
