199204c4288f1541bf2103f18107250f4a126f2e
dukekunyura <dukekunyura@gmail.com>
2023-12-29 10:01:38 +0300

    [*] fix import for vercel

--- src/components/optionsTree/OptionsTree.tsx
+++ src/components/optionsTree/OptionsTree.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import styles from "./OptionsTree.module.scss";
 import { PiFileTextFill } from "react-icons/pi";
 import { RiDeleteBinFill } from "react-icons/ri";
